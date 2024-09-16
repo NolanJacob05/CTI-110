f591a3fe6544afadc4a42dc7ab9188cd30abdcd4
NolanJacob05 <jacobn5153@student.faytechcc.edu>
2024-09-16 11:24:14 -0400

    First Upload

---
+++ P1LAB1_JacobNolan.py
@@ -0,0 +1,13 @@
+# Nolan Jacob
+# 9/16/24
+# P1LAB1
+# Get input from user and display output
+
+# Get first name from user.
+f_name = input("Enter your first name: ")
+
+# Get last name from user.
+l_name = input("Enter your last name: ")
+
+# Display info to user
+print("Hello,", f_name, l_name + "!", "Welcome to CTI-110 ")
